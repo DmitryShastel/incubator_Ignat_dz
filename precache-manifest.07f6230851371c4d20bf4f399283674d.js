fa4bc5f0f94b7bc1f6a284933088bf024e0e5606
DmitryShastel <dmshastel@gmail.com>
2021-01-24 12:36:05 +0300

    Updates

---
+++ precache-manifest.07f6230851371c4d20bf4f399283674d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705546d2ec46db703574a88b32248ae1",
+    "url": "/incubator_Ignat_dz/index.html"
+  },
+  {
+    "revision": "d5a5cc1a42cb96de915e",
+    "url": "/incubator_Ignat_dz/static/css/main.63cdff5b.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/incubator_Ignat_dz/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/incubator_Ignat_dz/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a5cc1a42cb96de915e",
+    "url": "/incubator_Ignat_dz/static/js/main.e791b26a.chunk.js"
+  },
+  {
+    "revision": "a5884a5dcb26695c7a49",
+    "url": "/incubator_Ignat_dz/static/js/runtime-main.c0ca7be8.js"
+  }
+]);
